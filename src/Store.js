2baf2e814ff015dbdbfdb3b29f66acb31019daf4
Shreyas-hub-coder <114660515+Shreyas-hub-coder@users.noreply.github.com>
2024-06-24 10:26:24 +0530

    redux

---
+++ src/Store.js
@@ -0,0 +1,16 @@
+import { createStore } from "redux";
+function cartReducer (state= { items: {}}, action) {
+    switch (action.type) {
+        case "ADD_TO_CART": {
+
+}
+        case "REMOVE_FROM_CART": {
+}
+    default:
+    return state;
+}
+I
+}
+
+const store = createStore (cartReducer);
+export default Store;
